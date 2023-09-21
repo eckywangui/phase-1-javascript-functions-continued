9764bf4bfc49d8d2feca590026715de9931bb981
Eckra Wangui <+>
2023-09-21 12:57:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+
 // code your solution here
+
+  function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun()); 
+
+const mondayWork= function(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+ console.log(mondayWork)
+
+
+ function wrapAdjective(programmer) {
+    return function(adjective = "special") {
+        return `You are ${programmer}${adjective}${programmer}!`;
+    };
+ }
+
